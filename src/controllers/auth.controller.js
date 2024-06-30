f5145542f0a3ecdc4cbaa7a766909830a16dc026
Eriktg30 <108430223+Eriktg30@users.noreply.github.com>
2024-06-30 15:58:33 -0600

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -67,14 +67,14 @@ export const login = async (req, res) => {
         if (!isMatch) return res.status(400).json({ msg: 'Datos incorrectos' })
 
         const token = await createAccessToken({ id: userFound._id })
-        // res.cookie('token', token)
+        res.cookie('token', token)
 
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: 'none',
-            path: '/'
-        });
+        // res.cookie("token", token, {
+        //     httpOnly: true,
+        //     secure: true,
+        //     sameSite: 'none',
+        //     path: '/'
+        // });
 
         res.status(200).json({
             id: userFound.id,
