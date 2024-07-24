58c3a685195af4acbbf1c04f5dfcb355ccf0589e
Eriktg30 <108430223+Eriktg30@users.noreply.github.com>
2024-07-24 13:18:17 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,6 +3,8 @@ import admin from './admin.js';
 import Emergencia from './models/emergencias.model.js';
 import Policia from './models/autoridades.model.js';
 import dotenv from 'dotenv';
+import { notificaciones } from './notificacion.js'
+
 
 dotenv.config();
 
@@ -19,44 +21,8 @@ export const connectDB = async () => {
         await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER_URL}/?retryWrites=true&w=majority&appName=${DB_NAME}`);
         console.log('>>> DB is connected <<<');
 
-        const db = mongoose.connection.db;
-        const collection = db.collection('emergencias');
-        const policiaCollection = db.collection('policias');
-
-        const changeStream = collection.watch();
-
-        changeStream.on('change', async (change) => {
-            if (change.operationType === 'update' && change.updateDescription.updatedFields.Policias) {
-                const emergenciaId = change.documentKey._id;
-                const policiaId = change.updateDescription.updatedFields.Policias;
-
-                // Encuentra el policía por su ID
-                const policia = await policiaCollection.findOne({ _id: new mongoose.Types.ObjectId(policiaId) });
-
-                if (!policia) {
-                    console.error(`Policía con ID ${policiaId} no encontrado`);
-                    return;
-                }
-
-                const tokenM = policia.notificationToken;
-
-                // Crea el payload de la notificación
-                const payload = {
-                    notification: {
-                        title: 'Nueva Emergencia Asignada',
-                        body: `Se te ha asignado una nueva emergencia con ID: ${emergenciaId}.`,
-                    },
-                };
-
-                // Envia la notificación
-                try {
-                    await admin.messaging().sendToDevice(tokenM, payload);
-                    console.log('Notificación enviada al policía:', policia.NombresPo);
-                } catch (error) {
-                    console.error('Error al enviar notificación:', error);
-                }
-            }
-        });
+        notificaciones()
+      
     } catch (error) {
         console.error('Error al conectar a la base de datos:', error);
     }
