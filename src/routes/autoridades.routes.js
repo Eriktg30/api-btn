bc4e9e25bb112e5dd832e3e43994de14fd4eb888
Eriktg30 <108430223+Eriktg30@users.noreply.github.com>
2024-07-27 18:40:46 -0600

    Update autoridades.routes.js

--- src/routes/autoridades.routes.js
+++ src/routes/autoridades.routes.js
@@ -1,16 +1,20 @@
 import { Router } from "express";
 
 import { authRequired } from '../middlewares/validateToken.js'
-import { getPolicia , getPolicias, updatePolicia} from "../controllers/autoridades.controller.js";
+import { getPolicia , getPolicias, updatePolicia, verifyToken, loginAutoridades} from "../controllers/autoridades.controller.js";
 
 const router = Router()
 
 
+router.post('/loginAutoridades', loginAutoridades)
 
 router.get('/emergenciasPP', getPolicias)
 
 router.get('/emergenciasPP/:id', getPolicia)
 
 router.put('/policia/:id', updatePolicia)
+
+router.get('/verifyPolicia', verifyToken)
+
 
 export default router
