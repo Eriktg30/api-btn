2ff4d5c21ab5cb8af68da51820e289ba94df9915
Eriktg30 <108430223+Eriktg30@users.noreply.github.com>
2024-07-27 18:44:48 -0600

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register, login, profile, logout, verifyToken, verifyTokenA } from "../controllers/auth.controller.js";
+import { register, login, profile, logout, verifyToken } from "../controllers/auth.controller.js";
 import { authRequired } from "../middlewares/validateToken.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 
@@ -12,8 +12,6 @@ router.post('/login', login)
 router.post('/logout', logout)
 
 router.get('/verify', verifyToken)
-
-router.get('/verifyA', verifyTokenA)
 
 router.post('/profile/:id', profile)
 
