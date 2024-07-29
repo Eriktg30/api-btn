e18f914c6597f980f3f91878c90b2dd88948ebb5
Eriktg30 <108430223+Eriktg30@users.noreply.github.com>
2024-07-29 16:55:54 -0600

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -95,6 +95,24 @@ export const login = async (req, res) => {
     }
 }
 
+export const updateProfile = async (req, res) => {
+    const { email, password, phone, phoneFamily, municipio } = req.body
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id,
+            { email, password, phone, phoneFamily, municipio },
+            { new: true }
+        )
+
+        if (!user)
+            return res.status(404).json({ msg: 'Emergencia no encontrada' })
+
+        return res.status(200).json({ msg: 'Actualizado', user })
+
+    } catch (error) {
+        return res.status(500).json({ msg: 'Error interno del servidor' })
+    }
+}
 
 export const profile = async (req, res) => {
 
