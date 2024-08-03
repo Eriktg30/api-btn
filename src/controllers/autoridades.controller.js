3b9431967264d764529685df76809c766ffde642
Eriktg30 <108430223+Eriktg30@users.noreply.github.com>
2024-08-03 14:38:59 -0600

    Update autoridades.controller.js

--- src/controllers/autoridades.controller.js
+++ src/controllers/autoridades.controller.js
@@ -30,11 +30,11 @@ export const getPolicia = async (req, res) => {
 }
 
 export const updatePolicia = async (req, res) => {
-    const { notificationToken } = req.body
+    const { notificationToken, EstadoPo } = req.body
 
     try {
         const policia = await Autoridades.findByIdAndUpdate(req.params.id,
-            { notificationToken},
+            { notificationToken, EstadoPo},
             { new: true }
         )
 
