92a10d110560b7e4de5c58a6e8070cf723e869d3
Eriktg30 <108430223+Eriktg30@users.noreply.github.com>
2024-08-08 18:19:04 -0600

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -15,7 +15,7 @@ router.get('/verify', verifyToken)
 
 router.put('/profile/:id', updateProfile)
 
-router.post('/profile/:id', authRequired, profile)
+router.get('/profile/:id', authRequired, profile)
 
 
 
