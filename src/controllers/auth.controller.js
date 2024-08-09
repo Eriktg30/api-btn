6bfc00cd02b67a2da23292cad42a06f33a2c3e6a
Eriktg30 <108430223+Eriktg30@users.noreply.github.com>
2024-08-08 20:21:59 -0600

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -109,7 +109,7 @@ export const updateProfile = async (req, res) => {
         return res.status(200).json({ msg: 'Actualizado', user })
 
     } catch (error) {
-        return res.status(500).json({ msg: 'Error interno del servidor' })
+        return res.status(500).json({ msg: 'Error interno del servidor actualizar' })
     }
 }
 
