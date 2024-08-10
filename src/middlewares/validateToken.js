e78f4fb19f662f34e266d0abf72dd96a11f04986
Eriktg30 <108430223+Eriktg30@users.noreply.github.com>
2024-08-10 16:04:51 -0600

    Update validateToken.js

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -3,7 +3,7 @@ import { TOKEN_SECRET } from '../config.js'
 
 export const authRequired = (req, res, next) => {
 
-    const { token } = req.cookies
+    const token = req.headers.authorization.split(' ')[1];
 
     if (!token)
         return res.status(401).json({ msg: 'no autorizado' })
