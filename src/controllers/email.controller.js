70922d96747e60af39e43dc087693f4066fe19e9
Eriktg30 <108430223+Eriktg30@users.noreply.github.com>
2024-08-12 15:00:00 -0600

    Update email.controller.js

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -42,6 +42,8 @@ export const verifyCode = async (req, res) => {
 
         const tiempotf =  Date.now() < userFound.codigoExpiracion
         const tiempo = Date.now()
+        const user = userFound.code
+        const usertf = userFound.code === code
 
         if (!userFound) 
             return res.status(500).json({ msg: 'usuario no encontrado' })
@@ -49,7 +51,7 @@ export const verifyCode = async (req, res) => {
         if(userFound.code === code && Date.now() < userFound.codigoExpiracion){
             return res.status(200).json({msg: 'código valido'})
         }else {
-            return res.status(400).json({ msg: 'Código inválido o expirado', email, code, tiempotf, tiempo })
+            return res.status(400).json({ msg: 'Código inválido o expirado', email, code, user, tiempotf, tiempo })
         }
         
     } catch (error) {
