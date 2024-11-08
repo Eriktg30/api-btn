e5a0b53c0d0deb3ea712d0390f620f0490d531e9
Eriktg30 <108430223+Eriktg30@users.noreply.github.com>
2024-11-07 23:47:38 -0600

    Update email.controller.js

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -31,6 +31,34 @@ export const sendCode = async (req, res) => {
     }
 }
 
+export const sendCodeCorreo = async (req, res) => {
+    const { email } = req.body
+
+    try {
+        const code = generarToken()
+        const expirationTime = Date.now() + 15 * 60 * 1000
+
+        const userFound = await User.findOneAndUpdate(
+            { email: email },
+            {
+                codigo: code,
+                codigoExpiracion: expirationTime
+            },
+            { new: true }
+        )
+
+        if (!userFound)
+            return res.status(404).json({ msg: 'El correo no esta registrado' })
+
+        await sendResetCode(email, code)
+
+        return res.status(200).json({ msg: '¡Código enviado! Por favor, revisa tu bandeja de entrada.' })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Error al enviar el código' })
+    }
+}
+
 export const verifyCode = async (req, res) => {
     const { email, code } = req.body
 
