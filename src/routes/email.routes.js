f41812e5ea63948a8b0151ebb976ca756e5c969c
Eriktg30 <108430223+Eriktg30@users.noreply.github.com>
2024-11-07 23:48:24 -0600

    Update email.routes.js

--- src/routes/email.routes.js
+++ src/routes/email.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-import { sendCode, verifyCode } from '../controllers/email.controller.js'
+import { sendCode, verifyCode, sendCodeCorreo } from '../controllers/email.controller.js'
 
 const router = Router()
 
 router.post('/generarCodigo', sendCode)
+
+router.post('/generarCodigoCorreo', sendCodeCorreo)
 
 router.post('/verifyCodigo', verifyCode)
 
